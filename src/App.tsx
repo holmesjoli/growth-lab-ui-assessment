f85e37087533097c64334f17afa4a50256f53f46
Joli Holmes <holmesjoli@gmail.com>
2023-10-14 13:32:34 -0700

    initial fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import './App.css';
 
+const url = "https://gist.githubusercontent.com/bleonard33/38a183289ed87082fed7b2547f2eea49/raw/3290b8ea9791c4e632520a9e1849f580bb82346a/census_classification.json"
+
+fetch(url, {method: 'GET'})
+  .then(response => response.json())
+  .then(body => {
+    console.log(body)
+});
+
+
 function App() {
   return (
     <div className="App">
